a83a77a7f0f00a8edf46578bb16693590f811014
ryuya tsuruta <byjsx117lp@yahoo.co.jp>
2020-09-11 17:12:39 +0900

    dd削除_20200911

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -21,8 +21,6 @@ class MoviesController extends Controller
 
     public function store(Request $request) {
 
-        dd($request);
-
         $this->validate($request, [
             'url' => 'required|max:11',
             'comment' => 'max:36', 
